01788de7b41abd0519b126265c7edf866ea07037
Sy Moen <tech@gallupcurrent.com>
2012-08-24 17:44:22 -0400

    add something useless

---
+++ class.ebayGetSingleItem.php
@@ -0,0 +1,11 @@
+<?php
+   /*
+   Plugin Name: eBay GetSingleItem
+   Plugin URI: 
+   Description: Embeds an ebay item view in a post via a short code
+   Version: 0.0.1
+   Author: Sy Moen
+   Author URI: https://github.com/moens
+   License: MIT
+   */
+?>
