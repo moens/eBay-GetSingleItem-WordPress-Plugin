e21a72281791f6c13d7ff9712291f60cb14f2899
Sy Moen <tech@gallupcurrent.com>
2012-08-24 20:05:29 -0400

    tom mcfarlane boilerplate... sweet

--- class.ebayGetSingleItem.php
+++ class.ebayGetSingleItem.php
@@ -1,11 +1,23 @@
 <?php
    /*
    Plugin Name: eBay GetSingleItem
-   Plugin URI: 
+   Plugin URI: https://github.com/moens/eBay-GetSingleItem-WordPress-Plugin
    Description: Embeds an ebay item view in a post via a short code
    Version: 0.0.1
    Author: Sy Moen
    Author URI: https://github.com/moens
    License: MIT
    */
+
+class ebayGetSingleItem {
+	function checkStatus(){
+		// do we have an eBay appid?
+	}
+	function 
+}
+
+
+
+
+
 ?>
