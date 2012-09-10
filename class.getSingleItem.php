406fecc09343d3fc210d1cc1dc8bbefdfa620f58
Sy Moen <tech@gallupcurrent.com>
2012-09-10 17:19:31 -0400

    separate view into its own class

--- class.getSingleItem.php
+++ class.getSingleItem.php
@@ -18,7 +18,7 @@ define('EBAPI_PLUGIN_DIR_PATH', plugin_dir_path( __FILE__ ));
 define('EBAPI_PLUGIN_DIR', substr(plugin_basename(__FILE__), 0, strpos(plugin_basename(__FILE__), '/') -1) ); // this is the name of the folder your plugin lives in
 
 if(!is_admin()) {	// frontend only
-
+	require_once( EBAPI_PLUGIN_DIR_PATH . 'class.getSingleItemView.php' );
 } else {	// admin only
 	require_once( EBAPI_PLUGIN_DIR_PATH . 'vendor/updater.php' ); // load the updater class that allows github updates, see checkGithubUpdates()
 
@@ -116,95 +116,10 @@ class ebayGetSingleItem { // extends WP_Widget { <-- this class is not a widget
 		$json = wp_remote_retrieve_body( wp_remote_get( $url ) );
 		$ebayItemData = json_decode( $json, TRUE );
 //echo'<pre>';print_r($ebayItemData);echo'</pre>';
+		
+		$getSingleItemView = new getSingleItemView;
+		$getSingleItemView->renderTemplate($ebayItemData);
 
-		//Get pertinent data:
-		// linkURL (string/url)
-		$linkURL = $ebayItemData['Item']['ViewItemURLForNaturalSearch'];
-
-		// title (string/text)
-		$title = $ebayItemData['Item']['Title'];
-
-		// description (string/html)
-		$description = stripslashes(html_entity_decode($ebayItemData['Item']['Description']));
-//print_r($description);
-		preg_match_all('/desc.*?dl.*?dd>(.*?)<\/dd/s', $description, $matchData);
-		if($matchData[1][0]) $description = $matchData[1][0];
-
-		// isEnded (bool)
-		$isEnded = ($ebayItamData['Item']['ListingStatus'] == 'Completed')?TRUE:FALSE;
-		if(!$isEnded) {
-			date_default_timezone_set('America/Denver');
-			$endingTimeStamp = strtotime($ebayItamData['EndTime']);
-			// endingTime
-			$endingTime = date('l, F jS Y T \a\t g:ia', $endingTimeStamp);
-			$durationVals = $this->parseDuration($ebayItamData['TimeLeft']);
-			$duration = '';
-			foreach($durationVals as $name => $interval) {
-				$durationText[] = $interval . ' ' . ( ($interval > 1)?$name:rtrim($name,'s') );
-			}
-			// duration
-			$duration = implode(', ', $durationText);
-		}
-		// bidPrice
-		$bidPrice = (isset($ebayItemData['Item']['ConvertedCurrentPrice']['Value']))?$ebayItemData['Item']['ConvertedCurrentPrice']['Value']:FALSE;
-		// isBuyItNow
-		$isBuyItNow = (isset($ebayItemData['Item']['BuyItNowAvailable']))?$ebayItemData['Item']['BuyItNowAvailable']:FALSE;
-		// buyItNowPrice
-		$buyItNowPrice = (isset($ebayItemData['Item']['ConvertedBuyItNowPrice']['Value']))?$ebayItemData['Item']['ConvertedBuyItNowPrice']['Value']:FALSE;
-		// listingType
-		$listingType = $ebayItemData['Item']['ListingType'];
-		// isSold
-		$isSold = ( !$isEnded ) ? FALSE : ( ( isset ( $ebayItemData['Item']['BidCount'] ) && ( $ebayItemData['Item']['BidCount'] > 0 ) ) ? TRUE : FALSE );
-		//Layout
-?>
-		<h2><a href="<?php echo $linkURL ?>"><?php echo $title ?></a></h2>
-		<div><?php echo $description ?></div>
-		<div class="price">
-			<?php _e('Current bid price:') ?> <?php echo '$' . $bidPrice . '.00' ?>
-<?php		if(!$isEnded): // show current bid, buy it now if available, end time and time left ?>
-<?php			if($isBuyItNow && $buyItNowPrice): ?>
-				&nbsp;&mdash;&nbsp;<?php _e('Or buy it now for:') ?> <?php echo $buyItNowPrice ?>
-<?php			endif; ?>
-<?php			if($listingType != 'FixedPriceItem' && $listingType != 'StoresFixedPrice'): ?>
-			<div class="timeLeft">
-				<?php echo __('Auction ends:') . ' ' . $endingTime . '(' . $duration . ')' ?>
-			</div>
-<?php			endif; ?>
-<?php		else: ?>
-			<div class="ended">
-				<p><?php echo __('This auction has ended,') . $isSold?__(' and is no longer available.'):__(' but we still have this item... <a href="/contact.html">contact us</a> for more details.') ?></p>
-			</div>
-<?php		endif; ?>
-		</div>
-		<div class="seeAuction"><a href="<?php echo $linkURL ?>">See Auction on eBay &raquo;</a></div>
-<?
-	}
-
-	/**
-	* Parse an ISO 8601 duration string
-	* @param string $str
-	* @return array
-	**/
-
-	function parseDuration($str)
-	{
-	$result = array();
-	preg_match('/^(?:P)([^T]*)(?:T)?(.*)?$/', trim($str), $sections);
-	if(!empty($sections[1])) {
-		preg_match_all('/(\d+)([YMWD])/', $sections[1], $parts, PREG_SET_ORDER);
-		$units = array('Y' => 'years', 'M' => 'months', 'W' => 'weeks', 'D' => 'days');
-		foreach($parts as $part) {
-			$result[$units[$part[2]]] = $part[1];
-		}
-	}
-	if(!empty($sections[2])) {
-		preg_match_all('/(\d+)([HMS])/', $sections[2], $parts, PREG_SET_ORDER);
-		$units = array('H' => 'hours', 'M' => 'minutes', 'S' => 'seconds');
-		foreach($parts as $part) {
-			$result[$units[$part[2]]] = $part[1];
-		}
-	}
-	return($result);
 	}
 
 	/*--------------------------------------------------*/
